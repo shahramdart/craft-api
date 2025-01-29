6223872b47ae643bc92538b7d862cb07db1a0d93
Shahram Othman Awla <108738123+shahramdart@users.noreply.github.com>
2025-01-29 11:29:21 +0300

    Changing db config to dotenv

--- Config/database.js
+++ Config/database.js
@@ -1,8 +1,15 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize("mobile-ranya", "root", "", {
-  host: "localhost",
+dotenv.config();
+
+const db = new Sequelize({
+  database: process.env.DATABASE,
+  host: process.env.HOST,
   dialect: "mysql",
+  password: process.env.PASSWORD,
+  username: process.env.USER,
+  port: process.env.PORT_DB,
 });
 
 export default db;
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import session from "express-session";
-import dotenv from "dotenv";
 import UserRoutes from "./Routes/users.routes.js";
 import AuthRoutes from "./Routes/auth.routes.js";
 import PermissionRoutes from "./Routes/permission.routes.js";
@@ -15,6 +14,7 @@ import sequelizeStore from "connect-session-sequelize";
 import SallingRoutes from "./Routes/salling.routes.js";
 import db from "./Config/database.js";
 import Expenses from "./Routes/expenses.routes.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -77,7 +77,7 @@ app.use("/api", SallingRoutes);
 // })();
 
 // Sync session store
-// store.sync(); // Sync session store with the database
+// store.sync();
 
 // Start the server
 app.listen(PORT, () => {
